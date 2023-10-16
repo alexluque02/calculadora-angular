33132a9b2a230725b3949a39d6f40b0b6e2fcfbd
alexluque02 <alexanderluquehoffrogge@gmail.com>
2023-10-16 20:00:48 +0200

    Calculadora hecha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CalculadoraAngular';
-  numeroAMostrar: number = 0;
+  numeroAMostrar: string = "0"; // Inicializamos con "0" como cadena
+  resultado: number = 0;
+  calculado: boolean = false;
+
   addToScreen(num: number) {
-    this.numeroAMostrar = num;
+    if (this.calculado) {
+      this.numeroAMostrar = "0"
+      this.calculado = false;
+    }
+    if (this.numeroAMostrar === "0") {
+      this.numeroAMostrar = num.toString();
+    } else {
+      this.numeroAMostrar += num.toString();
+    }
+  }
+
+  sumar() {
+    this.numeroAMostrar += "+";
+  }
+
+  restar() {
+    this.numeroAMostrar += "-";
+  }
+
+  calcular() {
+    let operaciones = this.numeroAMostrar.split(/(\+|-)/);
+    let resultado = 0;
+    let operador = '+';
+
+    operaciones.forEach((element) => {
+      if (element === '+') {
+        operador = '+';
+      } else if (element === '-') {
+        operador = '-';
+      } else {
+        let num = parseInt(element);
+        if (operador === '+') {
+          resultado += num;
+        } else if (operador === '-') {
+          resultado -= num;
+        }
+      }
+    });
+
+    this.numeroAMostrar = resultado.toString();
+    this.calculado = true;
+  }
+
+  borrar() {
+    this.numeroAMostrar = "0"
   }
 }
